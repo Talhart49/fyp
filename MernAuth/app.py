6e83d79443098c8fc8af724eb9144329036d9b52
Talha <ranatalhaa01@gmail.com>
2023-05-17 00:35:54 +0500

    recommendation done

--- MernAuth/app.py
+++ MernAuth/app.py
@@ -1,14 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import numpy as np
 import pandas as pd
-import ast
-import nltk
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import csv
 from pymongo import MongoClient
+import Levenshtein
+
 
 
 app = Flask(__name__)
@@ -49,8 +48,7 @@ webs = pd.read_csv('output.csv')
 print(webs.head())
 
 webs = webs[["id", "authorName", "authorEmail", "templateName", "templateDescription", 'templateStatus', 'Count', 'image', 'dateCreated']]
-# webs['templateName'] = webs['Category'] + " " + webs['cleaned_website_text']
-# webs = webs.drop(columns=['cleaned_website_text'])
+
 webs['templateDescription'] = webs['templateDescription'].apply(lambda x: x.lower())
 
 # Stem the words
@@ -70,12 +68,52 @@ vector = cv.fit_transform(webs['templateDescription']).toarray()
 similarity = cosine_similarity(vector)
 
 # Define the recommend function
+# def recommend(web):
+#     index = webs[webs['templateName'] == web].index[0]
+#     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+#     recommendations = []
+#     for i in distances[:6]:
+#         recommendations.append(webs.iloc[i[0]].id)
+#     return recommendations
+
+# def recommend(web):
+#     distances = []
+#     for i, template in webs.iterrows():
+#         template_name = template['templateDescription']
+#         similarity_score = Levenshtein.distance(web, template_name)
+#         distances.append((i, similarity_score))
+#     distances = sorted(distances, key=lambda x: x[1])
+#     recommendations = [webs.iloc[i]['id'] for i, _ in distances[:6]]
+#     return recommendations
+
+# def recommend(web):
+#     distances = []
+#     for i, template in webs.iterrows():
+#         templateDescription = template['templateDescription']
+#         similarity_score = 1 - (Levenshtein.distance(web, templateDescription) / max(len(web), len(templateDescription)))
+#         distances.append((i, similarity_score))
+#     distances = sorted(distances, key=lambda x: x[1], reverse=True)
+#     recommendations = [webs.iloc[i]['id'] for i, _ in distances[:6]]
+#     return recommendations
+
 def recommend(web):
-    index = webs[webs['templateDescription'] == web].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommendations = []
-    for i in distances[1:6]:
-        recommendations.append(webs.iloc[i[0]].id)
+    distances = []
+    input_words = set(web.split())  # Split input category into individual words
+
+    for i, template in webs.iterrows():
+        templateDescription = template['templateDescription']
+        template_words = set(templateDescription.split())  # Split template name into individual words
+        matched_words = input_words.intersection(template_words)  # Find common words
+
+        if len(matched_words) > 0:
+            similarity_score = len(matched_words) / len(input_words)
+        else:
+            similarity_score = 0
+
+        distances.append((i, similarity_score))
+
+    distances = sorted(distances, key=lambda x: x[1], reverse=True)
+    recommendations = [webs.iloc[i]['id'] for i, _ in distances[:6]]
     return recommendations
 
 # Define the API route for recommendations
